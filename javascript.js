8ab9dab8e9a0b9529927d9130e9d7f5159d5de62
Jack Williamson <jackwilliamson963@gmail.com>
2022-12-30 20:29:51 +0000

    Singal Game working

--- javascript.js
+++ javascript.js
@@ -1,37 +1,66 @@
-function getComputerChoice(){
+let scorePlayer = 0;
+let scoreComputer = 0;
+
+function getPlayerChoice(){
+     let choice = prompt();
+    return choice.charAt(0).toUpperCase() + choice.slice(1);
+      
+
+}
+
+
+function getComputerChoice() {
     const options = ['Rock', 'Paper', 'Scissors'];
     const randomIndex = Math.floor(Math.random() * options.length);
     return options[randomIndex];
 }
 
-function playRound(playerSelection, compterSelection){
-//Draw
-if (playerSelection === "Rock" && compterSelection === "Rock" ||
-    playerSelection === "Paper" && compterSelection === "Paper" ||
-    playerSelection === "Scissors" && compterSelection === "Scissors")
-return "The game is tied";
 
-//Win
-if (playerSelection === "Rock" && compterSelection === "Scissors" ||
-    playerSelection === "Paper" && compterSelection === "Rock" ||
-    playerSelection === "Scissors" && compterSelection === "Paper")
-return "You win, " + playerSelection + " Beats " + compterSelection;
+let playerSelection = getPlayerChoice();
+let compterSelection = getComputerChoice();
 
-//Lose
-if (playerSelection === "Rock" && compterSelection === "Paper" ||
-    playerSelection === "Paper" && compterSelection === "Scissors" ||
-    playerSelection === "Scissors" && compterSelection === "Rock")
-return "You Lose, " + compterSelection + " Beats " + playerSelection;
+function playRound(playerSelection, compterSelection) {
+    console.log("User Choice: " + playerSelection);
+    console.log("Computer Choice: " + compterSelection);
+   
 
+    //Draw
+    if (playerSelection === "Rock" && compterSelection === "Rock" ||
+        playerSelection === "Paper" && compterSelection === "Paper" ||
+        playerSelection === "Scissors" && compterSelection === "Scissors") {
+        return "The game is tied";
+    }
+
+    //Win
+    else if (playerSelection === "Rock" && compterSelection === "Scissors" ||
+        playerSelection === "Paper" && compterSelection === "Rock" ||
+        playerSelection === "Scissors" && compterSelection === "Paper") {
+        scorePlayer++;
+        return "You win, " + playerSelection + " Beats " + compterSelection;
+    }
+    //Lose
+    else if(playerSelection === "Rock" && compterSelection === "Paper" ||
+        playerSelection === "Paper" && compterSelection === "Scissors" ||
+        playerSelection === "Scissors" && compterSelection === "Rock") {
+        scoreComputer++;
+        return "You Lose, " + compterSelection + " Beats " + playerSelection;
+        }
 }
 
 
-let playerSelection = "Rock";
-//getPlayerChoice().charAt(0).toUpperCase() + getPlayerChoice().slice(1);
-let compterSelection = getComputerChoice();
+function game() {
+    for (i = 0; i < 5; i++) {
+        console.log(playRound(playerSelection, compterSelection));
+       
+        console.log("Scores");
+        console.log("User: " + scorePlayer);
+        console.log("Computer: " + scoreComputer);
+      
+    }
+}
 
+console.log(game());
 
-console.log(playRound(playerSelection, compterSelection));
 
 
 
