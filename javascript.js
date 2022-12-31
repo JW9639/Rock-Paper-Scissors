4aa4d0ac6645300953b1fbd89ba15512805a7d20
Jack Williamson <jackwilliamson963@gmail.com>
2022-12-31 17:20:26 +0000

    Displays results, game wont end when score hits 5

--- javascript.js
+++ javascript.js
@@ -2,6 +2,25 @@ let scorePlayer = 0;
 let scoreComputer = 0;
 
 
+const output = document.querySelector('#output');
+const player = document.createElement('div');
+const computer = document.createElement('div');
+const result = document.createElement('div');
+
+const Score = document.querySelector('#score');
+const playerScore = document.createElement('div');
+const computerScore = document.createElement('div');
+playerScore.classList.add('playerS');
+computerScore.classList.add('computerS');
+
+player.classList.add('player');
+computer.classList.add('computer');
+result.classList.add('result');
+
+
+
+const buttons = document.querySelectorAll('button');
+
 
 
 function getPlayerChoice() {
@@ -19,15 +38,16 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, compterSelection) {
-    console.log("\nUser Choice: " + playerSelection);
-    console.log("Computer Choice: " + compterSelection);
-
+    player.textContent = "\nUser Choice: " + playerSelection;
+    computer.textContent = "Computer Choice: " + compterSelection;
+    output.appendChild(player);
+    output.appendChild(computer);
 
     //Draw
     if (playerSelection === "Rock" && compterSelection === "Rock" ||
         playerSelection === "Paper" && compterSelection === "Paper" ||
         playerSelection === "Scissors" && compterSelection === "Scissors") {
-        return "\nThe game is tied";
+        return result.textContent = "\nThe game is tied";
     }
 
     //Win
@@ -35,55 +55,109 @@ function playRound(playerSelection, compterSelection) {
         playerSelection === "Paper" && compterSelection === "Rock" ||
         playerSelection === "Scissors" && compterSelection === "Paper") {
         scorePlayer++;
-        return "\nYou win, " + playerSelection + " Beats " + compterSelection;
+        return result.textContent = "\nYou win, " + playerSelection + " Beats " + compterSelection;
     }
     //Lose
     else if (playerSelection === "Rock" && compterSelection === "Paper" ||
         playerSelection === "Paper" && compterSelection === "Scissors" ||
         playerSelection === "Scissors" && compterSelection === "Rock") {
         scoreComputer++;
-        return "\nYou Lose, " + compterSelection + " Beats " + playerSelection;
+        return result.textContent = "\nYou Lose, " + compterSelection + " Beats " + playerSelection;
     }
+
 }
 
-function rock(){
+function rock() {
     let playerSelection = "Rock";
     let compterSelection = getComputerChoice();
-    playRound(playerSelection,compterSelection);
+    playRound(playerSelection, compterSelection);
+    output.appendChild(result);
+    Score.textContent = "\nScores";
+    playerScore.textContent = "User: " + scorePlayer;
+    computerScore.textContent = "Computer: " + scoreComputer;
+    Score.appendChild(playerScore);
+    Score.appendChild(computerScore);
 }
 
-function paper(){
+function paper() {
+
     let playerSelection = "Paper";
     let compterSelection = getComputerChoice();
-    playRound(playerSelection,compterSelection);
+
+    playRound(playerSelection, compterSelection);
+    output.appendChild(result);
+    Score.textContent = "\nScores";
+    playerScore.textContent = "User: " + scorePlayer;
+    computerScore.textContent = "Computer: " + scoreComputer;
+    Score.appendChild(playerScore);
+    Score.appendChild(computerScore);
+
 }
 
-function scissors(){
+function scissors() {
     let playerSelection = "Scissors";
     let compterSelection = getComputerChoice();
-    playRound(playerSelection,compterSelection);
+    playRound(playerSelection, compterSelection);
+    output.appendChild(result);
+
+    Score.textContent = "\nScores";
+    playerScore.textContent = "User: " + scorePlayer;
+    computerScore.textContent = "Computer: " + scoreComputer;
+    Score.appendChild(playerScore);
+    Score.appendChild(computerScore);
+
 }
 
-/*
+function gameOver(){
+    console.log("Game Over")
+    output.innerHTML = '<button class="start">New Game?</button>'
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+
+            if (button.id === "start") {
+              game();
+            }
+           
+        });
+    });
+}
+
+
 function game() {
-    for (i = 0; i < 5; i++) {
-        console.log("\nRound " + (i + 1))
-        let playerSelection = getPlayerChoice();
-        let compterSelection = getComputerChoice();
-        console.log(playRound(playerSelection, compterSelection));
-        console.log("\nScores");
-        console.log("User: " + scorePlayer);
-        console.log("Computer: " + scoreComputer);
-    }
-    if (scorePlayer > scoreComputer) {
-        console.log("\nYou have beat the computer, congrats!");
-    }
-    else console.log("\nYou have been beaten by the computer, maybe you shold try your luck again");
+  
 
+
+        buttons.forEach((button) => {
+            button.addEventListener('click', () => {
+
+
+                if (button.id === "Rock") {
+                    rock();
+                }
+                else if (button.id === "Paper") {
+                    paper();
+                }
+                else if (button.id === "Scissors") {
+                    scissors();
+                }
+            });
+        });
+
+       
+
+    
+    if (scorePlayer === 5) {
+        console.log("\nYou have beat the computer, congrats!");
+        gameOver();
+    }
+    else if (scoreComputer === 5){
+     console.log("\nYou have been beaten by the computer, maybe you shold try your luck again");
+     gameOver();
+    }
+   
 }
+
 
 game();
-
-*/
 
 
